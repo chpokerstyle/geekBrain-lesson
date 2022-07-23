cc7b67b55ca775cb487bd1b2819efc67e42d52f7
Волокитин Дмитрий Витальевич <volokitindv@admlr.lipetsk.ru>
2022-07-23 18:05:33 +0300

    Первое дз

--- src/main/java/course3/lesson1.java
+++ src/main/java/course3/lesson1.java
@@ -7,18 +7,17 @@ public class lesson1 {
     public static void main(String[] args) {
 
         String[] arr = {"asd", "ds", "ret", "123"};
-        //Integer[] arr = {3, 4, 6, 234, -1324, 111};
 
         System.out.println("Task 1\n" + Arrays.toString(arr));
         swapElements(arr, 2, 3);
         System.out.println(Arrays.toString(arr));
 
 
-        //Задача 2. Написать метод, который преобразует массив в ArrayList
         List<String> list = convertToList(arr);
         System.out.println("Task 2\n" + list.getClass() + " : " + list);
 
     }
+
     private static <T> void swapElements(T[] array, int index1, int index2) {
         T temp = array[index1];
         array[index1] = array[index2];
